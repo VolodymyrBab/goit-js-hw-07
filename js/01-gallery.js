185cf0b405bc0a4a02f3e6c249180594bba94405
VolodymyrBab <volbv@yahoo.com>
2023-04-01 21:26:31 +0200

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,49 +1,5 @@
-import { galleryItems } from './gallery-items.js';
+// Add imports above this line
+import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
-const galleryRef = document.querySelector('.gallery');
-// console.log(galleryRef);
-
-
-const makeGalleryMarkup = galleryItem => {
-  return `
-  <div class="gallery__item">
-    <a href="${galleryItem.original}" class="gallery__link">
-      <img
-        src="${galleryItem.preview}" 
-        alt="${galleryItem.description}" 
-        data-source="${galleryItem.original}"
-        class="gallery__image"
-        >
-    </a>
-  </div>`;
-};
-
-const makeGallery = galleryItems.map(makeGalleryMarkup).join('');
-galleryRef.insertAdjacentHTML('afterbegin', makeGallery);
-
-
-
-function onClickImage(evt) {
-  evt.preventDefault();
-  if (!evt.target.classList.contains('gallery__image')) {
-    return;
-  }
-  const imageLink = evt.target.dataset.source;
-  // console.log(imageLink);
-
-  const instance = basicLightbox.create(`
-		<img width="1200" src="${imageLink}" class="gallery__image">
-	`)
-  instance.show();
-
-
-  document.addEventListener('keydown', function (evt) {
-  if (evt.key === 'Escape') {
-    instance.close()
-  }
-});
-}
-
-galleryRef.addEventListener('click', onClickImage);
